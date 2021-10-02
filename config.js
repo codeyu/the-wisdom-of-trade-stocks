20c487b47ec09074b8da97ae5205e5c180a43139
codeyu <codeyu@gmail.com>
2021-10-02 18:46:42 +0900

    init

---
+++ config.js
@@ -0,0 +1,26 @@
+var CONFIG = {
+  // your website's title
+  document_title: "炒股的智慧",
+
+  // index page
+  index: "README.md",
+
+  // sidebar file
+  sidebar_file: "sidebar.md",
+
+  // where the docs are actually stored on github - so you can edit
+  base_url: "https://github.com/codeyu/the-wisdom-of-trade-stocks/edit/master",
+};
+
+// **************************
+// DON'T EDIT FOLLOWING CODES
+// **************************
+
+addConfig(ditto, CONFIG);
+
+function addConfig(obj, conf) {
+  Object.keys(conf).forEach(function (key) {
+    obj[key] = conf[key];
+  });
+}
+
---
+++ sidebar.md
@@ -0,0 +1,16 @@
+# [炒股的智慧]()
+
+作者：[陈江挺](https://book.douban.com/subject/1182173/)
+
+授权：<a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">署名-非商用许可证</a>
+
+## 目录
+1. [简介](#README)
+1. [前言](#docs/preface)
+1. [引子 三则故事](#docs/introduction)
+1. [第一章 炒股的挑战](#docs/the-challenge-of-trading-stocks)
+
+## 其他
+- [源码](https://github.com/codeyu/the-wisdom-of-trade-stocks)
+- [修订历史](https://github.com/codeyu/the-wisdom-of-trade-stocks/commits/master)
+- [反馈意见](https://github.com/codeyu/the-wisdom-of-trade-stocks/issues)
